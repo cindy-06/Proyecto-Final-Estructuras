6356bb45d22326ebce29bff03762d1e6d2974733
AngelAlejandroIn <AngelAlejandroIn@outlook>
2025-12-03 13:55:59 -0600

    Desarrollo del algoritmo BFS

--- tests/bfs_dfs_test.cpp
+++ tests/bfs_dfs_test.cpp
@@ -5,11 +5,33 @@
 
 using namespace std;
 
-void bfs(Graph& g, int startVertex) {
+void bfs(Graph &g, int startVertex)
+{
 	int v = g.getNumVertices();
-	vector <bool> visited(v, false);
+	vector<bool> visited(v, false);
 	queue<int> q;
 
 	visited[startVertex] = true;
 	q.push(startVertex);
+
+	cout << "BFS iniciando en " << startVertex << ": ";
+
+	while (!q.empty())
+	{
+		int current = q.front();
+		q.pop();
+		cout << current << " ";
+
+		const auto &adj = g.getAdjListRaw();
+		for (auto const &edge : adj[current])
+		{
+			int neighbor = edge.first;
+			if (!visited[neighbor])
+			{
+				visited[neighbor] = true;
+				q.push(neighbor);
+			}
+		}
+	}
+	cout << "\n";
 }
