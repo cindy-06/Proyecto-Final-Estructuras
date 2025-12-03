eee96b63094dea787ffb8999b32ce762b10f25a8
AngelAlejandroIn <AngelAlejandroIn@outlook>
2025-12-03 13:21:28 -0600

    Incluimos un struct Edge, que formará nuestro grafo, e indispensable para su funcionamiento

--- tests/GraphTest.h
+++ tests/GraphTest.h
@@ -8,6 +8,11 @@
 
 using namespace std;
 
+struct Edge
+{
+	int src, dest, weight;
+};
+
 class Graph
 {
 public:
@@ -24,60 +29,73 @@ private:
 		adjList.resize(vertices);
 	}
 
-	void addEdge(int src, int dest, int weight = 1) {
-        adjList[src].push_back({dest, weight});
-        edges.push_back({src, dest, weight});
+	void addEdge(int src, int dest, int weight = 1)
+	{
+		adjList[src].push_back({dest, weight});
+		edges.push_back({src, dest, weight});
 
-        if (!isDirected) {
-            adjList[dest].push_back({src, weight});
-        }
-    }
+		if (!isDirected)
+		{
+			adjList[dest].push_back({src, weight});
+		}
+	}
 
-int getNumVertices() const { return numVertices; }
-    const vector<list<pair<int, int>>>& getAdjListRaw() const { return adjList; }
+	int getNumVertices() const { return numVertices; }
+	const vector<list<pair<int, int>>> &getAdjListRaw() const { return adjList; }
 
-    void printAdjList() {
-        cout << "\n--- Lista de Adyacencia ---\n";
-        for (int i = 0; i < numVertices; ++i) {
-            cout << "Vertice " << i << ":";
-            for (auto const& edge : adjList[i]) {
-                cout << " -> (" << edge.first << ", peso: " << edge.second << ")";
-            }
-            cout << endl;
-        }
-    }
+	void printAdjList()
+	{
+		cout << "\n--- Lista de Adyacencia ---\n";
+		for (int i = 0; i < numVertices; ++i)
+		{
+			cout << "Vertice " << i << ":";
+			for (auto const &edge : adjList[i])
+			{
+				cout << " -> (" << edge.first << ", peso: " << edge.second << ")";
+			}
+			cout << endl;
+		}
+	}
 
-    vector<vector<int>> getAdjMatrix() {
-        vector<vector<int>> matrix(numVertices, vector<int>(numVertices, 0));
+	vector<vector<int>> getAdjMatrix()
+	{
+		vector<vector<int>> matrix(numVertices, vector<int>(numVertices, 0));
 
-        for (int i = 0; i < numVertices; i++) {
-            for (auto const& edge : adjList[i]) {
-                int dest = edge.first;
-                int weight = edge.second;
-                matrix[i][dest] = weight;
-            }
-        }
-        return matrix;
-    }
+		for (int i = 0; i < numVertices; i++)
+		{
+			for (auto const &edge : adjList[i])
+			{
+				int dest = edge.first;
+				int weight = edge.second;
+				matrix[i][dest] = weight;
+			}
+		}
+		return matrix;
+	}
 
-    vector<vector<int>> getIncidenceMatrix() {
-        int numEdges = edges.size();
-        vector<vector<int>> matrix(numVertices, vector<int>(numEdges, 0));
+	vector<vector<int>> getIncidenceMatrix()
+	{
+		int numEdges = edges.size();
+		vector<vector<int>> matrix(numVertices, vector<int>(numEdges, 0));
 
-        for (int j = 0; j < numEdges; j++) {
-            int u = edges[j].src;
-            int v = edges[j].dest;
+		for (int j = 0; j < numEdges; j++)
+		{
+			int u = edges[j].src;
+			int v = edges[j].dest;
 
-            if (isDirected) {
-                matrix[u][j] = 1;
-                matrix[v][j] = -1;
-            } else {
-                matrix[u][j] = 1;
-                matrix[v][j] = 1;
-            }
-        }
-        return matrix;
-    }
+			if (isDirected)
+			{
+				matrix[u][j] = 1;
+				matrix[v][j] = -1;
+			}
+			else
+			{
+				matrix[u][j] = 1;
+				matrix[v][j] = 1;
+			}
+		}
+		return matrix;
+	}
 };
 
 #endif
