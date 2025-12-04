cc0e559dc14e7bcc266b327d54f6a6f73d314912
Angel Castellanos <isaac.c141006@gmail.com>
2025-12-04 16:04:46 -0600

    Se modificaron los atributos de la clase Graph de private a protected para implementar herencia, se creo la clase hija Bipartite Graph donde se desarolla la lógica de coloreo

--- tests/GraphTest.h
+++ tests/GraphTest.h
@@ -15,7 +15,7 @@ struct Edge
 
 class Graph
 {
-private:
+protected:
 	int numVertices;
 	bool isDirected;
 	vector<list<pair<int, int>>> adjList;
@@ -41,6 +41,7 @@ public:
 	}
 
 	int getNumVertices() const { return numVertices; }
+	
 	const vector<list<pair<int, int>>> &getAdjListRaw() const { return adjList; }
 
 	void printAdjList()
@@ -96,6 +97,48 @@ public:
 		}
 		return matrix;
 	}
+};
+
+class BipartiteGraph : Graph{
+	private:
+		enum Color {red = 42, blu = 24, non = -1};
+
+		vector<bool> visited;
+		vector<int> color;
+		vector<int> predecesor;
+
+	public:
+		BipartiteGraph(int vertices, bool directed) : Graph(vertices, directed) {
+			visited = vector<bool>(vertices, false);
+			
+			color = vector<int> (vertices, non);
+			color[0] = red;
+
+			predecesor = vector<int> (vertices);
+			predecesor[0] = -1;
+		}
+
+		void color_graph(int v){
+			visited[v] = true;
+
+			if(predecesor[v] != -1){
+
+				color[v] = color[predecesor[v]] == red ? blu : red;
+				
+				cout << v << " con color: " << color[v];
+
+				for (auto const &edge : adjList[v])
+				{
+					int neighbor = edge.first;
+					if (!visited[neighbor])
+					{
+						color_graph(neighbor);
+					}
+				}
+			}
+		}
+
+		
 };
 
 #endif
